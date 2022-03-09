9dadbb642cf9fae7f657a5fb5bc8ff52b2b51954
akshat2109 <54941008+akshat2109@users.noreply.github.com>
2022-03-09 11:39:48 +0530

    Time: 30 ms (51.00%), Space: 10.6 MB (25.74%) - LeetHub

---
+++ 696-count-binary-substrings/696-count-binary-substrings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        int cur=1, pre=0, res=0;
+        
+        for(int i=1; i<s.size(); i++)
+        {
+            if(s[i-1] == s[i])
+                cur++;
+            else
+            {
+                res+=min(pre,cur), pre=cur, cur=1;
+            }
+        }
+        return res + min(pre,cur);
+    }
+};
